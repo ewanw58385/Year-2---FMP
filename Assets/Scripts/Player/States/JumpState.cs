0ff6cd0b00ce772ae13c9b84f43f45238e654837
ewanw58385 <72386462+ewanw58385@users.noreply.github.com>
2022-03-23 15:56:40 +0000

    Update JumpState.cs

--- Assets/Scripts/Player/States/JumpState.cs
+++ Assets/Scripts/Player/States/JumpState.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Threading.Tasks;
-
 public class JumpState : BaseState 
 {
     private Player_FSM _psm;
@@ -37,10 +35,11 @@ public class JumpState : BaseState
             horizontalInput = Input.GetAxisRaw("Horizontal"); //gets axis as vector2
             _psm.rb.velocity = new Vector2(horizontalInput * _psm.jumpMoveSpeed, _psm.rb.velocity.y); //applies velocity on the X axis while in the air without affecting Y velocity from jump
 
-        if(_psm.anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.3f && _psm.GroundCheck())
-        {
-            _psm.ChangeState(_psm.idle);
-        }
+        /* if(_psm.anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.3f && _psm.GroundCheck())
+         {
+             _psm.ChangeState(_psm.idle);
+         }*/
+
     }
 
     public override void Exit()
